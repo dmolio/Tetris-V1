f33c9c246c9f98da043ba7da0acdbfe5f54d56a5
dmolio <126358508+dmolio@users.noreply.github.com>
2024-07-30 15:32:10 -0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    profile = "default"
     bucket  = "dmolio-terraform"  # Removed the trailing space
     key     = "jenkins/terraform.tfstate"
     region  = "us-east-1"
